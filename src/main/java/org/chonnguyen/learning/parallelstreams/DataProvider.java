cd7d79054a2da1deb5bdf7b9b81290476adc54e6
chonnh <chonnh@gmail.com>
2017-06-08 20:32:22 +0700

    tet

--- src/main/java/org/chonnguyen/learning/parallelstreams/DataProvider.java
+++ src/main/java/org/chonnguyen/learning/parallelstreams/DataProvider.java
@@ -18,58 +18,59 @@ import java.util.logging.Logger;
 
 public class DataProvider {
 
-  private static final DataProvider _instance = new DataProvider();
-  private List<String> _surNames = new ArrayList<>();
-  private final Map<String, Gender> _givenNames = new HashMap<>();
-  private final Map<Integer, Article> _articles = new HashMap<>();
+    private static final DataProvider _instance = new DataProvider();
+    private List<String> _surNames = new ArrayList<>();
+    private final Map<String, Gender> _givenNames = new HashMap<>();
+    private final Map<Integer, Article> _articles = new HashMap<>();
 
-  public static DataProvider getInstance() {
-    return _instance;
-  }
+    public static DataProvider getInstance() {
+        return _instance;
+    }
 
-  private DataProvider() {
-    init();
-  }
+    private DataProvider() {
+        init();
+    }
 
-  public List<String> getSurNames() {
-    return _surNames;
-  }
+    public List<String> getSurNames() {
+        return _surNames;
+    }
 
-  public Map<String, Gender> getGivenNames() {
-    return _givenNames;
-  }
+    public Map<String, Gender> getGivenNames() {
+        return _givenNames;
+    }
 
-  public Map<Integer, Article> getArticles() {
-    return _articles;
-  }
+    public Map<Integer, Article> getArticles() {
+        return _articles;
+    }
 
-  private void init() {
-    try {
-      _surNames = readFile("Surnames.csv");
-      readFile("GivenNamesFemale.csv").stream().forEach(n -> _givenNames.put(n, Gender.Female));
-      readFile("GivenNamesMale.csv").stream().forEach(n -> _givenNames.put(n, Gender.Male));
-      int articleNo = 0;
-      for (String line : readFile("Articles.csv")) {
-        if (!line.trim().isEmpty()) {
-          articleNo++;
-          Article article = new Article(line, articleNo);
-          _articles.put(articleNo, article);
+    private void init() {
+        try {
+            _surNames = readFile("Surnames.csv");
+            readFile("GivenNamesFemale.csv").stream().forEach(n -> _givenNames.put(n, Gender.Female));
+            readFile("GivenNamesMale.csv").stream().forEach(n -> _givenNames.put(n, Gender.Male));
+            int articleNo = 0;
+            for (String line : readFile("Articles.csv")) {
+                if (!line.trim().isEmpty()) {
+                    articleNo++;
+                    Article article = new Article(line, articleNo);
+                    _articles.put(articleNo, article);
+                }
+            }
+            ;
+        } catch (IOException ex) {
+            Logger.getLogger(DataProvider.class.getName()).log(Level.SEVERE, null, ex);
         }
-      };
-    } catch (IOException ex) {
-      Logger.getLogger(DataProvider.class.getName()).log(Level.SEVERE, null, ex);
     }
-  }
 
-  private List<String> readFile(String fileName) throws IOException {
-    List<String> lines = new ArrayList<>();
-    try (InputStream is = getClass().getResourceAsStream(fileName);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is));) {
-      String line;
-      while ((line = reader.readLine()) != null) {
-        lines.add(line);
-      }
+    private List<String> readFile(String fileName) throws IOException {
+        List<String> lines = new ArrayList<>();
+        try (InputStream is = getClass().getResourceAsStream(fileName);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        }
+        return lines;
     }
-    return lines;
-  }
 }
